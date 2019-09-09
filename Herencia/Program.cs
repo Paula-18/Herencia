ef5d4794790d87a0a8cd83ce94cf8f3dc8ad271e
Paula-18 <paunahappy@gmail.com>
2019-09-09 12:38:47 -0700

    Cambios

--- Herencia/Program.cs
+++ Herencia/Program.cs
@@ -13,6 +13,34 @@ namespace Herencia
             Triangulo triangulo = new Triangulo();
             triangulo.Color = "Amarillo";
             Console.WriteLine(triangulo.toString());
+
+            triangulo.Altura = 2.5;
+            triangulo.Base = 4;
+
+            double areaTriangulo = triangulo.calcularArea();
+            Console.WriteLine("Area del triángulo: " + areaTriangulo);
+
+            Circulo circulo = new Circulo(5.4);
+            Console.WriteLine("Area del circulo: " + circulo.calcularArea().ToString());
+
+
+            //Polimorfismo: una clase se comporta como otra
+            Figura triangulo2 = new Triangulo();
+            ((Triangulo)triangulo2).Base = 2;
+            ((Triangulo)triangulo2).Altura = 3;
+            Console.WriteLine("Area del triánculo 2: " + triangulo2.calcularArea().ToString());
+
+            List<Figura> figuras = new List<Figura>();
+
+            figuras.Add(triangulo);
+            figuras.Add(circulo);
+            figuras.Add(triangulo2);
+
+            foreach(var figura in figuras)
+            {
+                Console.WriteLine("Area: " + figura.calcularArea().ToString());
+            }
+
             Console.Read();
         }
     }
